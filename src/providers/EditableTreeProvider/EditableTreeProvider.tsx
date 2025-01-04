bb4729b3db6c669eeb4c10d20774d05b36502778
Dariana Rengifo <darianarengifo@gmail.com>
2025-01-04 15:07:59 -0300

    [main] update (providers): documentation

--- src/providers/EditableTreeProvider/EditableTreeProvider.tsx
+++ src/providers/EditableTreeProvider/EditableTreeProvider.tsx
@@ -34,7 +34,14 @@ function EditableTreeProvider({ children }: EditableTreeProviderProps) {
     }
   }, []);
 
-  const syncTreeAndNodeMap = async (updatedTree: TreeNode) => {
+  /**
+   * Synchronizes the tree and node map with the updated tree structure.
+   * This function updates the state of the tree, the node map, and saves the updated tree to local storage.
+   *
+   * @param {TreeNode} updatedTree - The updated tree to be synced.
+   * @returns {Promise<void>} A promise that resolves once the synchronization is complete.
+   */
+  const syncTreeAndNodeMap = async (updatedTree: TreeNode): Promise<void> => {
     setTree(updatedTree);
     setNodeMap(buildNodeMap({ tree: [updatedTree] }));
     (await import("../../services/localStorage.service")).setItem(
@@ -43,6 +50,17 @@ function EditableTreeProvider({ children }: EditableTreeProviderProps) {
     );
   };
 
+  /**
+   * Adds a new node to the tree under a specified parent node.
+   * This function creates a new node and appends it as a child to the parent node.
+   * The updated tree is then synced with the state and local storage.
+   *
+   * @param {string} parentId - The ID of the parent node to which the new node should be added.
+   * @param {string} titleNode - The title of the new node to be added.
+   * @returns {Promise<void>} A promise that resolves once the node is added and the tree is synchronized.
+   *
+   * @throws {Error} Throws an error if the parent node is not found.
+   */
   const addNode: AddNodeFunction = useCallback(
     async (parentId, titleNode) => {
       const newNode: TreeNode = {
@@ -65,6 +83,16 @@ function EditableTreeProvider({ children }: EditableTreeProviderProps) {
     [nodeMap, tree]
   );
 
+  /**
+   * Edits an existing node in the tree with the provided updated data.
+   * This function updates the node's properties and then syncs the updated tree with the state and local storage.
+   *
+   * @param {string} nodeId - The ID of the node to be edited.
+   * @param {object} updatedData - The new data to update the node with.
+   * @returns {Promise<void>} A promise that resolves once the node is updated and the tree is synchronized.
+   *
+   * @throws {Error} Throws an error if the node to be edited is not found.
+   */
   const editNode: EditNodeFunction = useCallback(
     async (nodeId, updatedData) => {
       const nodeToEdit = nodeMap.get(nodeId);
@@ -79,6 +107,15 @@ function EditableTreeProvider({ children }: EditableTreeProviderProps) {
     [nodeMap, tree]
   );
 
+  /**
+   * Deletes a node from the tree based on the provided node ID.
+   * This function recursively searches for the node and removes it, then syncs the updated tree with the state and local storage.
+   *
+   * @param {string} nodeId - The ID of the node to be deleted.
+   * @returns {Promise<void>} A promise that resolves once the node is deleted and the tree is synchronized.
+   *
+   * @throws {Error} Throws an error if the node cannot be deleted or if the node is not found.
+   */
   const deleteNode: DeleteNodeFunction = useCallback(
     async (nodeId) => {
       const removeNode = (currentNode: TreeNode): TreeNode | null => {
@@ -101,11 +138,23 @@ function EditableTreeProvider({ children }: EditableTreeProviderProps) {
     [tree]
   );
 
+  /**
+   * Toggles the editable state of the tree.
+   * This function toggles whether the tree is in an editable state or not.
+   *
+   * @returns {void}
+   */
   const toggleEditable: ToggleFunction = useCallback(
     () => setEditableTree((prev) => !prev),
     []
   );
 
+  /**
+   * Toggles the expand/collapse state of all tree nodes.
+   * This function toggles whether all tree nodes should be expanded or collapsed.
+   *
+   * @returns {void}
+   */
   const toggleExpandAllTree: ToggleFunction = useCallback(
     () => setExpandAllTree((prev) => !prev),
     []
